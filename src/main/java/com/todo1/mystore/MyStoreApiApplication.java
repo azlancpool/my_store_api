0c1aead4b2a63b86cd1207af971a60ac7c662705
Cristian Naranjo <crispool@hotmail.com>
2022-01-10 08:31:55 -0500

    first commit

---
+++ src/main/java/com/todo1/mystore/MyStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.todo1.mystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo1/mystore/MyStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo1.mystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
