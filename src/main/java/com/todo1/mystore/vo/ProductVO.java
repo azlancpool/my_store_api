f77da0ca4436e61260e92a128be04eb985cd692d
Cristian Naranjo <crispool@hotmail.com>
2022-01-10 22:56:13 -0500

    Adding annotations in order to be used for validations on ProductVO

--- src/main/java/com/todo1/mystore/vo/ProductVO.java
+++ src/main/java/com/todo1/mystore/vo/ProductVO.java
@@ -3,6 +3,8 @@
  */
 package com.todo1.mystore.vo;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
@@ -16,10 +18,11 @@ public class ProductVO {
 	
 	private Long id;
 
-	@NotNull
+	@NotEmpty(message = "The product name must not be empty.")
 	private String name;
 
 	@NotNull
-	private int initialAmount;
+	@Min(value = 1, message = "The initial amount of a product must be upper than 1.")
+	private Integer initialAmount;
 
 }
